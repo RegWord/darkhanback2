1840978c42776ef66606af159532409726dca34c
RegWord <fernandezz228@mail.ru>
2025-02-11 16:54:02 +0500

    servak

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://darkhan2.vercel.app',
   })
 );
 
